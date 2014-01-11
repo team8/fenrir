a0c0b629aabc38f12e452051870056c80a49b49e
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-11 13:55:07 -0800

    Added some more structure that I think makes sense

--- Robot/Fenrir.cpp
+++ Robot/Fenrir.cpp
@@ -3,8 +3,17 @@
 Class Fenrir : public IterativeRobot {
  private:
 
- public:
+ public: //need to input stuff based on the names of stuff
+{
 
+}
+ //make sure we call the methods like this.
+  void AutonomousInit();
+  void AutonomousPeriodic();
+  void AutonomousDisabled();
+  void TeleopInit();
+  void TeleopPeriodic();
+  void TeleopDisabled();
 };
 
 Fenrir::Fenrir() {
