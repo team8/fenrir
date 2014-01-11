6aa8d5f87d3442d57ae3770c761a5572a87f21df
TPackard <tyler.packard86@gmail.com>
2014-01-11 13:40:20 -0800

    Added victors and encoders
    Still needs speed controllers

--- Robot/DriveTrain.h
+++ Robot/DriveTrain.h
@@ -1,7 +1,20 @@
 #include "Constants.h"
+#include <WPILib.h>
 
 class DriveTrain {
     private:
+    
+    // Victors
+    LinearVictor leftFrontVic;
+    LinearVictor leftBackVic;
+    LinearVictor rightFrontVic;
+    LinearVictor rightBackVic;
+    
+    // Encoders
+    Encoder leftEnc;
+    Encoder rightEnc;
+    
+    // Needs speed controller stuff
     
     public:
     
