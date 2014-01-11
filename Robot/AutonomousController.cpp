3a6dc21553a7341e53a12809010de0e916795e43
James Ngo <ducngocngo2@gmail.com>
2014-01-11 13:05:42 -0800

    added empty func bodies

--- Robot/AutonomousController.cpp
+++ Robot/AutonomousController.cpp
@@ -1,1 +1,32 @@
-//AutonomousController.cpp
+AutonomousController::AutonomousController(Robot *robotPointer)
+
+
+
+{
+  
+ this -> robot = robotPointer; 
+  
+}  
+
+AutonomousController::startTimer(){
+  
+  time.start();
+}
+
+AutonomousController::drive(){
+  
+  //we'll add this later
+  
+}
+
+AutonomousController::stop(){
+  
+  //we'll add this later
+  
+}
+
+AutonomousController::update(){
+  
+  //we'll add this later
+  
+}
