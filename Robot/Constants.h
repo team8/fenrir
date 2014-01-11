d63b75f68b99df5a1f425cdc2d1fd1eb14ba4354
battlefront123 <battlefront123@gmail.com>
2014-01-11 13:22:16 -0800

    add ports for drive train victors

--- Robot/Constants.h
+++ Robot/Constants.h
@@ -1,6 +1,11 @@
 
-
 //ports for HumanController
 #define PORT_SPEED 0;
 #define PORT_OPERATOR 0;
 #define PORT_TURN 0;
+
+//ports for drive train victors
+#define PORT_DRIVE_VIC_1 0;
+#define PORT_DRIVE_VIC_2 0;
+#define PORT_DRIVE_VIC_3 0;
+#define PORT_DRIVE_VIC_4 0;
