b8734ac47953e260ab5f302d95fe92cd182d78b3
James Ngo <ducngocngo2@gmail.com>
2014-01-11 15:14:17 -0800

    Update Robot.h

--- Robot/Robot.h
+++ Robot/Robot.h
@@ -1,7 +1,23 @@
+#include <WIPLib.h>
 #include "Constants.h"
+#include "AutonomousController.h"
+#include "DriveTrain.h"
+#include "HumanController.h"
+
 
 class Robot {
+    
     private:
     
+      DriveTrain driveTrain;
+      
+      
+    
     public:
+    
+    void setSpeed(float s);
+    
+    void rotateSpeed(float s);
+    
+    
 }
