50e88e867f77ec7ee99639c898a205b39b1e6644
TPackard <tyler.packard86@gmail.com>
2014-01-11 13:46:51 -0800

    Added victors and encoders
    Needs encoder ports and speed controllers

--- Robot/DriveTrain.cpp
+++ Robot/DriveTrain.cpp
@@ -1,1 +1,15 @@
-//drivetrain.cpp
+#include "DriveTrain.h"
+
+DriveTrain::DriveTrain() :
+
+        // Victors
+        leftFrontVic(PORT_DRIVE_VIC_1),
+        leftBackVic(PORT_DRIVE_VIC_2),
+        rightFrontVic(PORT_DRIVE_VIC_3),
+        rightBackVic(PORT_DRIVE_VIC_4),
+        
+        // Encoders
+        leftEnc(), // Needs port
+        rightEnc() // Needs port
+        
+        // Needs speed controller stuff
