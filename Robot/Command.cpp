4617b790957b9a3ae6c60bff083cb64da24f6ffa
TPackard <tyler.packard86@gmail.com>
2014-01-12 16:54:44 -0800

    Fixed to work in terminal

--- Robot/Command.cpp
+++ Robot/Command.cpp
@@ -1,20 +1,22 @@
-class Command {
-    public:
-    Command();
-};
+using namespace std;
 
-Command::Command() {
-    
-    enum commandType{DRIVE, SHOOT};
-    
-}
+enum commandType{DRIVE, SHOOT};
+ 
+void drive();
+void shoot();
 
 void command(int type){ // Use one of the command type enums here
     if (type == DRIVE) {
         drive();
+    } else if (type == SHOOT) {
+        shoot();
     }
 }
 
 void drive() {
-    
+    // Do driving stuff
+}
+
+void shoot() {
+    // Do shooting stuff
 }
