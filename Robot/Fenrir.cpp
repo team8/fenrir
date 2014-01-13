62e61679a03854b353cf3616d0ea1bf9da3063e3
AKotik <andrewkotik1@gmail.com>
2014-01-12 16:41:42 -0800

    Added method stubs and constructors.

--- Robot/Fenrir.cpp
+++ Robot/Fenrir.cpp
@@ -2,45 +2,80 @@
 
 class Fenrir : public IterativeRobot {
  private:
+         HumanController humanController;
+         AutonomousMode mode;
+         AutonomousController autoController;
+         Robot robot;
 
  public: //need to input stuff based on the names of stuff
 
  //make sure we call the methods like this.
+  void RobotInit();
   void AutonomousInit();
   void AutonomousPeriodic();
   void AutonomousDisabled();
+  void DisabledInit();
+  void DisabledPeriodic();
   void TeleopInit();
   void TeleopPeriodic();
   void TeleopDisabled();
+  void TestInit();
+  void TestPeriodic();
 };
 
-Fenrir::Fenrir() {
-
+Fenrir::Fenrir() :
+        humanController(&robot),
+        mode(DEFAULT),
+        autoController(&robot, mode),
+        robot()
+{
 }
 
-void Fenfir:: AutonomousInit(){
+void Fenrir:: RobotInit(){
  
 }
 
-void Fenfir:: AutonomousPeriodic(){
+void Fenrir:: AutonomousInit(){
  
 }
 
-void Fenfir:: AutonomousDisabled(){
-}
-
-void Fenfir:: TeleopInit(){
+void Fenrir:: AutonomousPeriodic(){
  
 }
 
-void Fenfir:: TeleopPeriodic(){
+void Fenrir:: AutonomousDisabled(){
  
 }
 
-void Fenfir:: TeleopPeriodic(){
+void Fenrir:: DisabledInit(){
  
 }
-void Fenfir:: TeleopDisabled(){
+
+void Fenrir:: DisabledPeriodic(){
+ 
+}
+
+void Fenrir:: TeleopInit(){
+ 
+}
+
+void Fenrir:: TeleopPeriodic(){
+ 
+}
+
+void Fenrir:: TeleopPeriodic(){
+ 
+}
+
+void Fenrir:: TeleopDisabled(){
+ 
+}
+
+void Fenrir:: TestInit(){
+ 
+}
+
+void Fenrir:: TestPeriodic(){
  
 }
 
