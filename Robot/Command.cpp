ae95a50193e5336052665c1244a6e3812382d3a6
TPackard <tyler.packard86@gmail.com>
2014-01-14 14:14:45 -0800

    Moved stuff to Command.h

--- Robot/Command.cpp
+++ Robot/Command.cpp
@@ -1,20 +1,5 @@
 #include "Constants.h"
-
-class Command {
-    public:
-        Command();
-        Command(int subsystemEnum, int methodEnum, int newPassValue);
-        void command(int subsystemEnum, int methodEnum, int newPassValue);
-        int[] getCommand();
-        int getSubsystem();
-        int getMethod();
-        int getPassValue();
-    private:
-        int subsystem; // Subsystem number. Defined in the commandType enum in Constants.h
-        int method; // Method number. Defined in an enum in Constants.h
-        int passValue; // Value passed to the method
-        int values [3];
-};
+#include "Command.h"
 
 Command::Command() {
     
