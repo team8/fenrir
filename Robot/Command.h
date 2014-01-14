65a286cd88e6b8ca7dd2c3834454c7c52cf27842
TPackard <tyler.packard86@gmail.com>
2014-01-14 15:43:28 -0800

    Update Command.h

--- Robot/Command.h
+++ Robot/Command.h
@@ -6,13 +6,11 @@ class Command {
         Command();
         Command(int subsystemEnum, int methodEnum, int newPassValue);
         void command(int subsystemEnum, int methodEnum, int newPassValue);
-        int[] getCommand();
         int getSubsystem();
         int getMethod();
         int getPassValue();
     private:
         int subsystem; // Subsystem number. Defined in the commandType enum in Constants.h
         int method; // Method number. Defined in an enum in Constants.h
-        int passValue; // Value passed to the method
-        int values [3];
-};
+        void *passArray; // Value passed to the method
+}
