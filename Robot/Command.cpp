c6976e2c22e6318d7ed0ca8cd65d6ce3a27cbdf2
TPackard <tyler.packard86@gmail.com>
2014-01-13 18:10:55 -0800

    Added pass value

--- Robot/Command.cpp
+++ Robot/Command.cpp
@@ -3,33 +3,37 @@
 class Command {
     public:
         Command();
-        Command(int, int);
-        void command(int);
+        Command(int subsystemEnum, int methodEnum, int newPassValue);
+        void command(int subsystemEnum, int methodEnum, int newPassValue);
         int[] getCommand();
         int getSubsystem();
         int getMethod();
+        int getPassValue();
     private:
-        int subsystem;
-        int method;
-        int values [2];
+        int subsystem; // Subsystem number. Defined in the commandType enum in Constants.h
+        int method; // Method number. Defined in an enum in Constants.h
+        int passValue; // Value passed to the method
+        int values [3];
 };
 
 Command::Command() {
     
 }
 
-Command::Command(int subsystemEnum, int methodEnum) {
+Command::Command(int subsystemEnum, int methodEnum, int newPassValue) {
     subsystem = subsystemEnum;
     method = methodEnum;
+    passValue = newPassValue;
 }
 
-void command(int subsystemEnum, int methodEnum){ // Use one of the command type enums here
+void command(int subsystemEnum, int methodEnum, int newPassValue){ // Use one of the command type enums here
     subsystem = subsystemEnum;
     method = methodEnum;
+    passValue = newPassValue;
 }
 
 int[] getCommand() {
-    values = {subsystem, method};
+    values = {subsystem, method, passValue};
     return values;
 }
 
@@ -39,4 +43,8 @@ int getSubsystem() {
 
 int getMethod() {
     return method;
+}
+
+int getPassValue() {
+    return passValue;
 }
