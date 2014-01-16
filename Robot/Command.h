d49282b570ec5b85919b23805effde67c26f7bac
TPackard <tyler.packard86@gmail.com>
2014-01-15 16:14:34 -0800

    Update Command.h

--- Robot/Command.h
+++ Robot/Command.h
@@ -1,16 +1,19 @@
 enum CommandType{DRIVE};
 enum DriveMethod{DRIVESPEED, DRIVEDIST, ROTATEANGLE, ROTATESPEED};
 
+typedef struct {
+    double value;
+} DriveArgs;
+
 class Command {
     public:
         Command();
-        Command(int subsystemEnum, int methodEnum, int newPassValue);
-        void command(int subsystemEnum, int methodEnum, int newPassValue);
+        Command(int subsystemEnum, int methodEnum, void * args);
+        void command(int subsystemEnum, int methodEnum, void * args);
         int getSubsystem();
         int getMethod();
-        int getPassValue();
+        void * argPointer; // Points to argument struct
     private:
         int subsystem; // Subsystem number. Defined in the commandType enum in Constants.h
         int method; // Method number. Defined in an enum in Constants.h
-        void *passArray; // Value passed to the method
 }
