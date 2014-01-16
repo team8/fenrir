fe1c804cc6b72c4d8380d08b42b9cfa2ec7fb1f2
battlefront123 <battlefront123@gmail.com>
2014-01-16 15:14:36 -0800

    add motors and void methods

--- Robot/Shooter.h
+++ Robot/Shooter.h
@@ -1,5 +1,19 @@
+#include "WPILib.h"
+
 class Shooter {
- public:
-  Shooter();
+ 
  private:
+  
+  Victor motor1;
+  Victor motor2;
+  Victor motor3;
+  Victor motor4;
+  
+ public:
+ 
+  void shoot();
+  void update();
+ 
+  Shooter();
+ 
 };
