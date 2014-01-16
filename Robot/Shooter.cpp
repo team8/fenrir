48330afb4d1edb92ef4aa6101b2f4f264693351f
TPackard <tyler.packard86@gmail.com>
2014-01-16 15:24:55 -0800

    Update Shooter.cpp

--- Robot/Shooter.cpp
+++ Robot/Shooter.cpp
@@ -1,15 +1,13 @@
-#include "Constants.h"
 #include "Shooter.h"
 
 Shooter::Shooter(): 
   
-  motor1(PORT_SHOOTER_VIC_1),
-  motor2(PORT_SHOOTER_VIC_2),
-  motor3(PORT_SHOOTER_VIC_3),
-  motor4(PORT_SHOOTER_VIC_4)
+  vic(PORT_SHOOTER_VIC_1),
+  vic2(PORT_SHOOTER_VIC_2),
+  vic3(PORT_SHOOTER_VIC_3),
+  vic4(PORT_SHOOTER_VIC_4)
 
 {
-
 
 }
 
