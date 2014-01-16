4b29b8866c77c6a225a30a87c47c2845ea9c50f7
TPackard <tyler.packard86@gmail.com>
2014-01-16 15:25:00 -0800

    Update Shooter.h

--- Robot/Shooter.h
+++ Robot/Shooter.h
@@ -1,19 +1,19 @@
 #include "WPILib.h"
+#include "Constants.h"
 
 class Shooter {
  
  private:
   
-  Victor motor1;
-  Victor motor2;
-  Victor motor3;
-  Victor motor4;
+  Victor vic1;
+  Victor vic2;
+  Victor vic3;
+  Victor vic4;
   
  public:
  
   void shoot();
   void update();
- 
   Shooter();
  
 };
