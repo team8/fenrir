87ae0ebebb15b9c35e0e61c7c215b76609607fea
JoeyKL <joey@kellison-linn.org>
2014-01-15 16:47:11 -0800

    SubsytemType

--- Robot/Command.cpp
+++ Robot/Command.cpp
@@ -5,13 +5,13 @@ Command::Command() {
     
 }
 
-Command::Command(int subsystemEnum, int methodEnum, void * args) {
+Command::Command(SubsystemType subsystemEnum, int methodEnum, void * args) {
     subsystem = subsystemEnum;
     method = methodEnum;
     argPointer = args;
 }
 
-void command(int subsystemEnum, int methodEnum, void * args){ // Use one of the command type enums here
+void command(SubsystemType subsystemEnum, int methodEnum, void * args){ // Use one of the command type enums here
     subsystem = subsystemEnum;
     method = methodEnum;
     argPointer = args;
