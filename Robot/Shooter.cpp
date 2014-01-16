415be5f63e4406b25a6e43c3d50dc9d2ec7e6533
battlefront123 <battlefront123@gmail.com>
2014-01-16 15:20:21 -0800

    add motors and void methods

--- Robot/Shooter.cpp
+++ Robot/Shooter.cpp
@@ -1,7 +1,25 @@
 #include "Constants.h"
 #include "Shooter.h"
 
-Shooter::Shooter(){
+Shooter::Shooter(): 
+  
+  motor1(PORT_SHOOTER_VIC_1),
+  motor2(PORT_SHOOTER_VIC_2),
+  motor3(PORT_SHOOTER_VIC_3),
+  motor4(PORT_SHOOTER_VIC_4)
+
+{
 
 
+}
+
+
+void Shooter::shoot(){
+  
+  
+}
+
+void Shooter::update(){
+  
+  
 }
