d71b0e4c3518f2a581154737cb98f657ac3463ca
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-16 16:39:16 -0800

    I succumbed to peer pressure (Johhny, Lilia, Kevin, Tyler)

--- Robot/DriveTrain.cpp
+++ Robot/DriveTrain.cpp
@@ -19,12 +19,14 @@ DriveTrain::DriveTrain() :
 
 //runs method according to what newCommand is received
 void DriveTrain::runMethod(Command newCommand) {
-        DriveArgs * args = (DriveArgs*) newCommand.argPointer;
+        DriveArgs* args = (DriveArgs*) newCommand.argPointer;
         if (command.getMethod() == SETSPEED) {
                 setSpeed(args -> driveSpeed);
-        } else if (command.getMethod() == DRIVEDIST) {
+        }
+        else if (command.getMethod() == DRIVEDIST) {
                 driveD(args -> driveDist);
-        } else if (command.getMethod() == ROTATEANGLE) {
+        }
+        else if (command.getMethod() == ROTATEANGLE) {
                 rotateA(args -> rotAngle);
         }
         free(args);
@@ -33,25 +35,23 @@ void DriveTrain::runMethod(Command newCommand) {
 void DriveTrain::update() {
   
 }
-void DriveTrain::driveD(double dist){
-    
+void DriveTrain::driveD(double dist) {
         
 }
 
 //sets the spd of all vics to the specified amount b/w 1.0 and -1.0
 //this method only makes the robot go staight, sorry guys.
-void DriveTrain::setSpeed(double spd){
+void DriveTrain::setSpeed(double spd) {
     leftFrontVic.set(-spd);
     leftBackVic.set(-spd);
     rightFrontVic.set(spd);
     rightBackVic.set(spd);
-    
 }
 
 //lets you rotate in place
 void DriveTrain::rotateA(double angle){
 	bool mode = Command.getParent(); //true if call is from autonomous, false if command is from teleop
-	if(mode==false){
+	if (mode == false){
 		//turns the specified number of degrees
 	}
 	else {
@@ -62,13 +62,13 @@ void DriveTrain::rotateA(double angle){
 //speed < 0 then turns LEFT
 //speed > 0 then turns RIGHT
 void DriveTrain::rotate(double speed) {
-	if(speed >0) {
+	if (speed > 0) {
 		leftFrontVic.set(leftFrontVic.get()-speed);
 	   	leftBackVic.set(leftBackVic.get()-speed);
    		rightFrontVic.set(rightFrontVic.get()-speed);
    		rightBackVic.set(rightBackVic.get()-speed);
 	}    	
-	if(speed < 0) {
+	if (speed < 0) {
 		leftFrontVic.set(leftFrontVic.get()+speed);
     	leftBackVic.set(leftBackVic.get()+speed);
     	rightFrontVic.set(rightFrontVic.get()+speed);
