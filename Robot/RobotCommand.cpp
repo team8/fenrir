3cdfb16c52a4a42e5490daf70dc9d73be16badf8
JoeyKL <joey@kellison-linn.org>
2014-01-20 14:11:07 -0800

    Removed inAuto

--- Robot/RobotCommand.cpp
+++ Robot/RobotCommand.cpp
@@ -1,17 +1,6 @@
 #include "Constants.h"
 #include "RobotCommand.h"
 
-RobotCommand::RobotCommand(bool inAuto) { // inAuto should be true if the Command is sent from autonomous, otherwise it is false
-    parent = inAuto;
-}
-
-RobotCommand::RobotCommand(bool inAuto, SubsystemType subsystemEnum, int methodEnum, void * args) {
-    subsystem = subsystemEnum;
-    method = methodEnum;
-    argPointer = args;
-    parent = inAuto;
-}
-
 void RobotCommand::command(SubsystemType subsystemEnum, int methodEnum, void * args){ // Use one of the command type enums here
     subsystem = subsystemEnum;
     method = methodEnum;
@@ -24,8 +13,4 @@ SubsystemType RobotCommand::getSubsystem() {
 
 int RobotCommand::getMethod() {
     return method;
-}
-
-bool RobotCommand::getParent() {
-    return parent;
 }
