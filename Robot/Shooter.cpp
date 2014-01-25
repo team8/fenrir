73e5ed518c7040ba2b3402ef345f236ec27c325a
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-24 16:23:58 -0800

    Fixed Indentation

--- Robot/Shooter.cpp
+++ Robot/Shooter.cpp
@@ -1,14 +1,13 @@
 #include "Shooter.h"
 
 Shooter::Shooter(): 
-  
-        shooterVic1(PORT_SHOOTER_VIC_1),
-        shooterVic2(PORT_SHOOTER_VIC_2),
-        shooterVic3(PORT_SHOOTER_VIC_3),
-        shooterVic4(PORT_SHOOTER_VIC_4),
-
-        loaderVic1(PORT_LOADER_VIC_1),
-        loaderVic2(PORT_LOADER_VIC_2)
+	shooterVic1(PORT_SHOOTER_VIC_1),
+	shooterVic2(PORT_SHOOTER_VIC_2),
+	shooterVic3(PORT_SHOOTER_VIC_3),
+	shooterVic4(PORT_SHOOTER_VIC_4),
+	
+	loaderVic1(PORT_LOADER_VIC_1),
+	loaderVic2(PORT_LOADER_VIC_2)
 {
 }
 
@@ -19,22 +18,22 @@ void Shooter::runCommand(RobotCommand command){
 }
 
 void Shooter::update(state){
-        switch(state) {
-                case IDLE:
-                        setAllVics(0);
-                        break;
-                case LOADED:
-                        state = PREP;
-                        break;
-                case PREP:
-                        // Aim the shooter and start spinning up
-						setShooterVics(SHOOTER_VICS_SPEED);
-						state = SHOOTING;
-						break;
-                case SHOOTING:
-                        shoot();
-                        state = IDLE;
-                        break;
+	switch(state) {
+		case IDLE:
+			setAllVics(0);
+			break;
+		case LOADED:
+			state = PREP;
+			break;
+		case PREP:
+			// Aim the shooter and start spinning up
+			setShooterVics(SHOOTER_VICS_SPEED);
+			state = SHOOTING;
+			break;
+		case SHOOTING:
+			shoot();
+			state = IDLE;
+		break;
 	}
 }
 
