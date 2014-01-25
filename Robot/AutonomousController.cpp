6a7733334ee63dcda732bca3863cc6d61895a546
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-24 16:35:20 -0800

    Fixed indentation

--- Robot/AutonomousController.cpp
+++ Robot/AutonomousController.cpp
@@ -2,38 +2,36 @@
 #include "Constants.h"
 
 AutonomousController::AutonomousController(Robot *robotPointer)
-
 //instance variables go here
-
 {
-  this -> robot = robotPointer;
+	this -> robot = robotPointer;
 }  
 
 void AutonomousController::startTimer(){
-  
-  time.Start();
+	time.Start();
 }
 
 void AutonomousController::drive(){
-  /* Needs to take variables, this is just an example
-  void * argPointer = malloc(sizeof(DriveArgs));
-  ((DriveArgs *) argPointer) -> speedValue = 23;
-  Method method;
-  method.driveMethod = SETSPEED;
-  Command command(DRIVE, method, argPointer);
-  robot -> setCommand(command);
-  */
+	/* Needs to take variables, this is just an example
+	void * argPointer = malloc(sizeof(DriveArgs));
+	((DriveArgs *) argPointer) -> speedValue = 23;
+	Method method;
+	method.driveMethod = SETSPEED;
+	Command command(DRIVE, method, argPointer);
+	robot -> setCommand(command);
+	*/
 }
 
 void AutonomousController::stop(){
-  void * argPointer = malloc(sizeof(DriveArgs));
-  ((DriveArgs *) argPointer) -> driveSpeed = 0;
-  Method method;
-  method.driveMethod = SETSPEED;
-  RobotCommand command(DRIVE, method, argPointer);
-  robot -> setCommand(command);
+	void * argPointer = malloc(sizeof(DriveArgs));
+	((DriveArgs *) argPointer) -> driveSpeed = 0;
+	Method method;
+	method.driveMethod = SETSPEED;
+	RobotCommand command(DRIVE, method, argPointer);
+	robot -> setCommand(command);
 }
 
 void AutonomousController::update(){
-  //we'll add this later
+	//Runs all methods according to time
+	
 }
