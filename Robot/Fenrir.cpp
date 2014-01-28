4b0c8e54f42d9ab0a02a407dde7e9de20c3fdc5b
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-28 15:10:08 -0800

    Fixed unneccesary stuff

--- Robot/Fenrir.cpp
+++ Robot/Fenrir.cpp
@@ -2,34 +2,32 @@
 #include "Robot.h"
 #include "HumanController.h"
 #include "AutonomousController.h"
-// fenrir.cpp
 
 class Fenrir : public IterativeRobot {
- private:
-         HumanController humanController;
-         AutonomousController autoController;
-         Robot robot;
+	private:
+		HumanController humanController;
+		AutonomousController autoController;
+		Robot robot;
 
- public: //need to input stuff based on the names of stuff
-        Fenrir();
- //make sure we call the methods like this.
-	void RobotInit();
-	void AutonomousInit();
-	void AutonomousPeriodic();
-	void AutonomousDisabled();
-	void DisabledInit();
-	void DisabledPeriodic();
-	void TeleopInit();
-	void TeleopPeriodic();
-	void TeleopDisabled();
-	void TestInit();
-	void TestPeriodic();
+	public:
+		Fenrir();
+		void RobotInit();
+		void AutonomousInit();
+		void AutonomousPeriodic();
+		void AutonomousDisabled();
+		void DisabledInit();
+		void DisabledPeriodic();
+		void TeleopInit();
+		void TeleopPeriodic();
+		void TeleopDisabled();
+		void TestInit();
+		void TestPeriodic();
 };
 
-Fenrir::Fenrir() :
-        humanController(&robot),
-        autoController(&robot),
-        robot()
+Fenrir::Fenrir():
+	humanController(&robot),
+	autoController(&robot),
+	robot()
 {
 
 }
@@ -75,11 +73,11 @@ void Fenrir::TeleopDisabled(){
 }
 
 void Fenrir::TestInit(){
- 
+	
 }
 
 void Fenrir::TestPeriodic(){
- 
+	
 }
 
 START_ROBOT_CLASS(Fenrir);
