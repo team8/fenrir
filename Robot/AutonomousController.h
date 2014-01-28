605bf2c2e675ab102417432360430d9d4761eb81
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-28 14:10:52 -0800

    Fixed indentation

--- Robot/AutonomousController.h
+++ Robot/AutonomousController.h
@@ -7,21 +7,21 @@
 
 class AutonomousController {
 
- public:
-  AutonomousController(Robot* robotPointer);
-  void update();
-  void startTimer();
-  void rotateDegrees();
-
- private:
- 
-  Timer time; 
-  Robot* robot;
-  void drive();
-  void stop();
-  bool shoot();
-  Ultrasonic ultraLeft(ECHO_PULSE_OUT_1, TRIGGER_PULSE_IN_1);
-  Ultrasonic ultraRight(ECHO_PULSE_OUT_2, TRIGGER_PULSE_IN_2);
-  bool targetReached;
+	public:
+		AutonomousController(Robot* robotPointer);
+		void update();
+		void startTimer();
+		void rotateDegrees();
+	
+	private:
+	
+		Timer time; 
+		Robot* robot;
+		void drive();
+		void stop();
+		bool shoot();
+		Ultrasonic ultraLeft(ECHO_PULSE_OUT_1, TRIGGER_PULSE_IN_1);
+		Ultrasonic ultraRight(ECHO_PULSE_OUT_2, TRIGGER_PULSE_IN_2);
+		bool targetReached;
 };
 #endif
