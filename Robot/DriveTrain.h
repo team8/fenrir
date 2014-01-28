15005998f5d0948ec991f30b6393df2230aaedeb
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-28 15:04:21 -0800

    Update DriveTrain.h

--- Robot/DriveTrain.h
+++ Robot/DriveTrain.h
@@ -20,59 +20,56 @@ typedef enum DriveMethod {
 } DriveMethod;
 
 class DriveTrain {
-    private:
-    
-    // Victors 
-    Victor leftFrontVic;
-    Victor leftBackVic;
-    Victor rightFrontVic;
-    Victor rightBackVic;
-    
-    Gyro gyroscope;
-    
-    // Encoders
-    Encoder leftEnc;
-    Encoder rightEnc;
+	private:
+	 // Victors 
+		Victor leftFrontVic;
+		Victor leftBackVic;
+		Victor rightFrontVic;
+		Victor rightBackVic;
+		
+		Gyro gyroscope;
+		
+		
+		Encoder leftEnc;
+		Encoder rightEnc;
 
-    //PID Controllers
-    PIDController leftController;
-    PIDController rightController;
-    PIDController angleController;
+		PIDController leftController;
+		PIDController rightController;
+		PIDController angleController;
     
-    //target speeds 
-    double targetSpeed;
-    double rotateSpeed;
-    double rotateAngle;
+		//target speeds 
+		double targetSpeed;
+		double rotateSpeed;
+		double rotateAngle;
     
-    //state 
-    DriveState state;
+		//state 
+		DriveState state;
+	
+	public:
+		DriveTrain();
     
-    
-    public:
-    DriveTrain();
-    
-    //Takes input of distance to destination
-    //Will use PID or PIDControllers to calculate input for victors. 
-    //Uses encoders as input for distance from destination
-    void driveD(float dist);
-    
-    //Takes input of -1 to 1 and sets speed of victors. 
-    //Only drives straight
-    void setSpeed(double spd);
-    
-    //this is a work in progress, right now, it doesn't do anything, what we want it to do is call rotateS and rotate 
-    //until the target angle has been reached. 
-    //I'm not sure if there is going to be a default speed for this rotate thing. :P
-    void rotateA(double angle);
-    
-    //takes current speed, and subtracts speed one one side and increases on another. -1 is left 1 is right.
-    //can someone check the logic to this? thanks
-    void rotateS(double speed);
-    
-    //Stops victors
-    void stopVictors();
-    
-    void update();
-    void runCommand(RobotCommand command);
+		/*Takes input of distance to destination
+		//Will use PID or PIDControllers to calculate input for victors. 
+		//Uses encoders as input for distance from destination*/
+		void driveD(float dist);
+	
+		//Takes input of -1 to 1 and sets speed of victors. 
+    	//Only drives straight
+		void setSpeed(double spd);
+		
+		/*this is a work in progress, right now, it doesn't do anything,
+		what we want it to do is call rotateS and rotate 
+		until the target angle has been reached. 
+    	I'm not sure if there is going to be a default speed for this rotate thing. :P*/
+		void rotateA(double angle);
+		
+		//takes current speed, and subtracts speed one one side and increases on another. -1 is left 1 is right.
+		//can someone check the logic to this? thanks
+		void rotateS(double speed);
+		
+		//Stops victors
+		void stopVictors();
+		void update();
+		void runCommand(RobotCommand command);
 };
 #endif
