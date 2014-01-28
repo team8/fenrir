f8ba3e6ba95e9fbef40c84aeba9d8dc0b4fcc05c
TPackard <tyler.packard86@gmail.com>
2014-01-27 17:03:53 -0800

    Added rotation method

--- Robot/AutonomousController.cpp
+++ Robot/AutonomousController.cpp
@@ -1,10 +1,13 @@
 #include "AutonomousController.h"
 #include "Constants.h"
+#include <stdlib.h>
 
 AutonomousController::AutonomousController(Robot *robotPointer)
 //instance variables go here
 {
 	this -> robot = robotPointer;
+	ultraLeft.SetAutomaticMode(false); // Prevents sensors from automatically going off
+	ultraRight.SetAutomaticMode(false);
 }  
 
 void AutonomousController::startTimer(){
@@ -36,4 +39,28 @@ void AutonomousController::stop(){
 void AutonomousController::update(){
 	//Runs all methods according to time
 	
+}
+
+void AutonomousController::rotateDegrees() { // Returns the amount of degrees of rotation needed to be perpendicular to the wall
+	ultraLeft.Ping(); // Sends ping
+	if (ultraLeft.IsRangeValid()) {
+		int leftDist = ultraLeft.GetRangeInches();
+		ultraRight.Ping();
+		if (ultraRight.IsRangeValid()) {
+			int rightDist = ultraRight.GetRangeInches();
+			double angle = atan((leftDist - rightDist)/ULTRA_GAP);
+			
+			// Sends Command
+			void * argPointer = malloc(sizeof(DriveArgs));
+			((DriveArgs *) argPointer) -> rotAngle = angle;
+			Method method;
+			method.driveMethod = ROTATEANGLE;
+			Command command(DRIVE, method, argPointer);
+			robot -> setCommand(command);
+			return;
+		}
+		printf("Right Ultrasonic - Invalid range\n");
+		return;
+	}
+	printf("Left Ultrasonic - Invalid range\n");
 }
