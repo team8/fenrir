1cad342e8fabb3556a2c3192b5d70207c39695b6
niharmitra <nihar.roy.mitra@gmail.com>
2014-01-28 14:17:28 -0800

    Fixed formatting

--- Robot/RobotCommand.cpp
+++ Robot/RobotCommand.cpp
@@ -1,18 +1,19 @@
 #include "Constants.h"
 #include "RobotCommand.h"
 
-	RobotCommand::RobotCommand(SubsystemType subsystemType, Method methodType, void * args){
-		// Use one of the command type enums here
-    subsystem = subsystemType;
-    method = methodType;
-    argPointer = args;
+	RobotCommand::RobotCommand(SubsystemType subsystemType, Method methodType, void * args) {
+	
+	// Use one of the command type enums here
+	subsystem = subsystemType;
+	method = methodType;
+	argPointer = args;
 }
 
 SubsystemType RobotCommand::getSubsystem() {
-    return subsystem;
+	return subsystem;
 }
 
 Method RobotCommand::getMethod() {
-    return method;
+	return method;
 }
 
