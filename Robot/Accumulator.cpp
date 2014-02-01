31f88c181a1a20ce091f596c401117577234972e
TPackard <tyler.packard86@gmail.com>
2014-01-31 16:32:20 -0800

    Removed unneccessary vics

--- Robot/Accumulator.cpp
+++ Robot/Accumulator.cpp
@@ -2,16 +2,7 @@
 
 Accumulator::Accumulator():
 	//accumulateVic((uint32_t)PORT_ACCUMULATOR_VIC_7)
-	accumulateVic((uint32_t) 1),
-	accumulateVic2((uint32_t) 2),
-	accumulateVic3((uint32_t) 3),
-	accumulateVic4((uint32_t) 4),
-	accumulateVic5((uint32_t) 5),
-	accumulateVic6((uint32_t) 6),
-	accumulateVic7((uint32_t) 7)
-
-
-	
+	accumulateVic((uint32_t) 3)
 {
 
 }
@@ -42,13 +33,6 @@ void Accumulator::update(){
 		break;
 	case ACCUMULATING:
 		accumulateVic.Set(0.5);
-		accumulateVic2.Set(0.5);
-		accumulateVic3.Set(0.5);
-		accumulateVic4.Set(0.5);
-		accumulateVic5.Set(0.5);
-		accumulateVic6.Set(0.5);
-		accumulateVic7.Set(0.5);
-
 		break;
 	case PASSING:
 		accumulateVic.Set(-0.5);
