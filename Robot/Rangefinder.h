194bef1b4af5c7dc7bb092727e8b38a75c6eb79e
TPackard <tyler.packard86@gmail.com>
2014-01-31 16:46:51 -0800

    Create Rangefinder.h

---
+++ Robot/Rangefinder.h
@@ -0,0 +1,21 @@
+#ifndef RANGEFINDER_H
+#define RANGEFINDER_H
+#include <WPILib.h>
+#include <math.h>
+#include "Robot.h"
+#include "Constants.h"
+#include "RobotCommand.h"
+#include "Drivetrain.h"
+
+class Rangefinder {
+	private:
+		Ultrasonic ultraLeft;
+		Ultrasonic ultraRight;
+
+	public:
+		void rotateDegrees();
+		int wallDist();
+		Rangefinder(Robot* robotPointer);
+};
+
+#endif
