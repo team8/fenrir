ce70d7182e7d9098fff9b1f9e5765b62515aef0b
Lilia Tang <liliatangxy@gmail.com>
2014-02-06 15:32:31 -0800

    modified autonomous drive and stop

--- Robot/AutonomousController.cpp
+++ Robot/AutonomousController.cpp
@@ -12,20 +12,15 @@ void AutonomousController::startTimer(){
 }
 
 void AutonomousController::drive(){
-	DriveArgs* argPointer -> speedValue = 23;
-	Command command(DRIVE, SETSPEED, argPointer);
+	DriveArgs* argPointer -> speedValue = ; // add some speed value
+	RobotCommand command(DRIVE, SETSPEED, argPointer);
 	robot -> setCommand(command);
 }
 
 void AutonomousController::stop(){
-	/* void * argPointer = malloc(sizeof(DriveArgs));
-	((DriveArgs *) argPointer) -> driveSpeed = 0;
-	Method method;
-	method.driveMethod = SETSPEED;
-	RobotCommand command(DRIVE, method, argPointer);
+	DriveArgs * argPointer -> driveSpeed = 0;
+	RobotCommand command(DRIVE, SETSPEED, argPointer);
 	robot -> setCommand(command);
-	*/
-	
 }
 
 void AutonomousController::update(){
